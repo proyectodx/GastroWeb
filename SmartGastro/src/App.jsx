4d62740999464f9691960e223215157f6f1c0c53
arley <cardozomontenegroarley@gmail.com>
2024-07-29 00:30:40 -0500

    agregar: implementa un filtro manual para la lista de productos

--- SmartGastro/src/App.jsx
+++ SmartGastro/src/App.jsx
@@ -1,8 +1,26 @@
-import { products } from "./mocks/products.json";
+import { products as initialProducts } from "./mocks/products.json";
 import { Products } from "./components/Products.jsx";
+import { useState } from "react";
 
 function App() {
-  return <Products products={products} />;
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const getFilteredProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = getFilteredProducts(products);
+
+  return <Products products={filteredProducts} />;
 }
 
 export default App;
