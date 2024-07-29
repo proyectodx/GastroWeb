5dec44281a55505e579b527130221ee0b9142faf
arley <cardozomontenegroarley@gmail.com>
2024-07-29 01:40:34 -0500

    Agregar: Implementar la funcionalidad de filtro de precios en el componente Filtros
    - Se agregó el gancho useState para administrar el estado del precio mínimo
    - Se implementó la función handleChangeMinPrice para actualizar el precio mínimo
    - Se mostró el precio mínimo seleccionado junto al rango de entrada

--- SmartGastro/src/components/Filters.jsx
+++ SmartGastro/src/components/Filters.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
+import "./Filters.css";
 export function Filters() {
+  const [minPrice, setMinPrice] = useState(0);
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+  };
+
   return (
     <section className="filters">
       <div className="filters__container">
-        <label htmlFor="price">Precio</label>
-        <input type="range" id="price" min="0" max="1000" />
-        <div>
-          <label htmlFor="category">Categoría</label>
-          <select id="category">
-            <option value="all">Todas</option>
-            <option value="laptops">Portátiles</option>
-            <option value="phones">Celulares</option>
-          </select>
-        </div>
+        <label htmlFor="price">Precio minimo</label>
+        <input
+          type="range"
+          id="price"
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
+      </div>
+      <div>
+        <label htmlFor="category">Categoría</label>
+        <select id="category">
+          <option value="all">Todas</option>
+          <option value="laptops">Portátiles</option>
+          <option value="phones">Celulares</option>
+        </select>
       </div>
     </section>
   );
