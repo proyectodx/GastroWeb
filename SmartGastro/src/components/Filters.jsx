5be122fe4c54fd51d6868c6fbb3443c5a27fee46
arley <cardozomontenegroarley@gmail.com>
2024-07-29 02:39:58 -0500

    Solucion: Sincronizar estado de filtros en el componente Filters

--- SmartGastro/src/components/Filters.jsx
+++ SmartGastro/src/components/Filters.jsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 import "./Filters.css";
-export function Filters() {
+
+export function Filters({ changeFilters }) {
   const [minPrice, setMinPrice] = useState(0);
+
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value);
+    const newMinPrice = event.target.value;
+    setMinPrice(newMinPrice);
+    changeFilters((prevState) => ({
+      ...prevState,
+      minPrice: newMinPrice,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    changeFilters((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
   };
 
   return (
     <section className="filters">
       <div className="filters__container">
-        <label htmlFor="price">Precio minimo</label>
+        <label htmlFor="price">Precio mínimo</label>
         <input
           type="range"
           id="price"
           min="0"
           max="1000"
+          value={minPrice}
           onChange={handleChangeMinPrice}
         />
         <span>${minPrice}</span>
       </div>
       <div>
         <label htmlFor="category">Categoría</label>
-        <select id="category">
+        <select id="category" onChange={handleChangeCategory}>
           <option value="all">Todas</option>
           <option value="laptops">Portátiles</option>
           <option value="phones">Celulares</option>
