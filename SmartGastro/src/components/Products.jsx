ffcead29d02d9f5aef7c2fad81a006d8f3970071
arley <cardozomontenegroarley@gmail.com>
2024-07-28 22:17:15 -0500

    agregar el precio a los productos

--- SmartGastro/src/components/Products.jsx
+++ SmartGastro/src/components/Products.jsx
@@ -9,7 +9,7 @@ export function Products({ products }) {
           <li key={product.id}>
             <img src={product.thumbnail} alt={product.title} />
             <div>
-              <strong>{product.title}</strong>
+              <strong>{product.title}</strong> - ${product.price}
             </div>
             <div>
               <button>
